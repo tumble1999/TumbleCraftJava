2e0b0d5f04d0e35732c3f1e0633b41512c2055da
Tumble <tumblegamer@gmail.com>
2018-04-09 22:29:16 +0100

    server auto-save

--- SYNC.bat
+++ SYNC.bat
@@ -3,7 +3,10 @@
 title GitHub Auto Save
 set status=git status
 set fetch=git fetch
+set add=git add
+set commit=git commit
 set pull=git pull
+set push=git push
 
 ping localhost>nul
 %status%
@@ -12,8 +15,23 @@ echo FETCHING...
 echo.
 echo.
 %status%
+echo ADDING...
+%add% .
+echo.
+echo.
+%status%
+echo COMMITING...
+%commit% -m "server auto-save"
+echo.
+echo.
+%status%
 echo PULLING
 %pull%
+echo.
+echo.
+%status%
+echo PUSHING...
+%push%
 echo.
 echo.
 goto :loop
