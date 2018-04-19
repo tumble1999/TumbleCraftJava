3d305f17ea3ec45fd51ac1ecaeb4e01eeec1b8c8
Tumble <tumblegamer@gmail.com>
2018-04-19 17:10:01 +0100

    server auto-save

--- START.bat
+++ START.bat
@@ -1,19 +1,33 @@
 @echo off
 del restart.tmp
 del restart.bat
-call gsync.bat
+rem call gsync.bat
+echo [Pretend] GitHub Sync
+pause
 title [Server] TumbleCraft
 
-(echo @echo off
+(rem echo @echo off
 echo echo.^>restart.tmp
+echo goto :eof
 )>restart.bat
 
 :restart
-call spigot.bat
-call gsync.bat
+echo [Pretend] Server Started
+pause
+start restart.bat
+echo [Pretend] Someone asked to restart the Server
+pause
+rem call spigot.bat
+rem call gsync.bat
+echo [Pretend] GitHub Sync
+pause
 if exist restart.tmp (
 del restart.tmp
+echo [Pretend] Server Restarting
+pause
 goto :restart
 )
+echo [Pretend] Server Stopping
 del restart.bat
+pause
 goto :eof
