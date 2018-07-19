fc4323f61f671413f3351912db962f373798d4d2
Tumble <tumblegamer@gmail.com>
2018-07-19 17:49:14 +0100

    server auto-save

--- bin/spigot.bat
+++ bin/spigot.bat
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
+
 @echo off
 rem java -Xmx1G -Xms1G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar paperclip.jar
 
-
-=======
-@echo off
-rem java -Xmx1G -Xms1G -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar paperclip.jar
-
-
->>>>>>> 19ea0d724b46aa07675a1a73e358fae3088862b2
 java -Xmx1G -Xms1G -jar spigot.jar
